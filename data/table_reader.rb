f990959478cad03624b7851df366497b4cd11840
Conan Tzou <conan@asperasoft.com>
2017-05-27 15:13:21 -0700

    add notes

--- data/table_reader.rb
+++ data/table_reader.rb
@@ -7,9 +7,17 @@ NCCI  = 'NCCI'.freeze
 SIC   = 'SIC'.freeze
 WCIRB = 'CA WC'.freeze
 
+SYSTEMS = [NAICS, NCCI, SIC, WCIRB].freeze
+
 # Description Types
 ISO_D = 'ISO Description'.freeze
 GEN_D = 'General Description'.freeze
+
+DESCRIPTIONS = [ISO_D, GEN_D].freeze
+
+# Matchers
+NA_REGEX  = /N\/A/
+VAR_REGEX = /Var\./
 
 header_lines = 0
 lines = 0
@@ -20,18 +28,24 @@ CSV.foreach('./fastcompclasscodecrossreferenceguide.csv',
             row_sep: "\n",
             skip_lines: /\AFastComp\.com/,
             skip_blanks: true) do |row|
-  if row['ISO Description'] && row['ISO Description'].match('Adhesive Mfg.')
-    p row
-  end
+  # if row['ISO Description'] && row['ISO Description'].match('Adhesive Mfg.')
+  #   p row
+  # end
 
-  if row['ISO Description'] == 'ISO Description'
-    # p row
-    header_lines += 1
-  end
+  p row if row[WCIRB] && row[WCIRB].match(VAR_REGEX)
+
+  header_lines += 1 if row[ISO_D] == ISO_D
   lines += 1
+
+  # cases to account for
 end
 
 if DEBUG_MODE
   puts "number of lines read: #{lines}"
   puts "number of header lines read: #{header_lines}"
 end
+
+# NOTE
+# ISO and General Descriptions are never N/A
+# some classification systems contain Var.
+# some classification systems contain N/A
