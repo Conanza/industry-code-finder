752e608370969c1b1fdb5e60d7e087ccfe909d7f
Conan Tzou <conan@asperasoft.com>
2017-05-27 15:58:47 -0700

    take note of special cases in parsed pdf

--- data/table_reader.rb
+++ data/table_reader.rb
@@ -28,16 +28,19 @@ CSV.foreach('./fastcompclasscodecrossreferenceguide.csv',
             row_sep: "\n",
             skip_lines: /\AFastComp\.com/,
             skip_blanks: true) do |row|
-  # if row['ISO Description'] && row['ISO Description'].match('Adhesive Mfg.')
-  #   p row
-  # end
+  p row if row[ISO_D] && row[ISO_D].match('Alarms and Alarm Systems')
 
-  p row if row[WCIRB] && row[WCIRB].match(VAR_REGEX)
+  # p row if row[WCIRB] && row[WCIRB].match(VAR_REGEX)
 
   header_lines += 1 if row[ISO_D] == ISO_D
   lines += 1
 
   # cases to account for
+  # 1. when a description contains a newline
+  #    a. it's either run long
+  #    b. or there are multiple descriptions mapping to different codes (should have matching newlines in codes)
+  # 2. when a description runs long and occupies a new row by itself
+  # 3. when an ISO Description maps to many other codes/descriptions and has white space on top and bottom
 end
 
 if DEBUG_MODE
