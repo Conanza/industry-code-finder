a0d6469efcba0f7e6c7c9d6ab457d39bd710cc33
Conan Tzou <conan@asperasoft.com>
2017-05-28 15:42:05 -0700

    start to implement algorithm for script

--- data/table_reader.rb
+++ data/table_reader.rb
@@ -19,34 +19,66 @@ description_types = [ISO_D, GEN_D]
 NA_REGEX  = /N\/A/
 VAR_REGEX = /Var\./
 
+# Helpers
+def space(str)
+  str[-1] == '-' ? '' : ' '
+end
+
 header_lines = 0
 lines = 0
+non_header_lines = 0
+CSV.open('./writer_test.csv', 'wb') do |csv|
+  deferred_rows = []
+  prev_row = nil
+  centered_row = nil
+  CSV.foreach('./fastcompclasscodecrossreferenceguide.csv',
+              headers: true,
+              col_sep: ',',
+              row_sep: "\n",
+              skip_lines: /\AFastComp\.com/,
+              skip_blanks: true) do |row|
+    lines += 1
+    if row[ISO_D] == ISO_D
+      header_lines += 1
+      next
+    end
+    non_header_lines += 1
 
-CSV.foreach('./fastcompclasscodecrossreferenceguide.csv',
-            headers: true,
-            col_sep: ',',
-            row_sep: "\n",
-            skip_lines: /\AFastComp\.com/,
-            skip_blanks: true) do |row|
-  # p row if row[ISO_D] && row[ISO_D].match('Alarms and Alarm Systems')
-  # p row if row[WCIRB] && row[WCIRB].match(VAR_REGEX)
+    if row[ISO_D].nil?
+      puts "no #{ISO_D}"
+      deferred_rows.push(row)
+    else
+      puts "#{ISO_D} exists"
+      row[ISO_D].gsub!("\n", " ")
 
-  p row if row['ISO CGL'] && row['ISO CGL'].match(NA_REGEX)
+      deffered_rows.length
+    end
 
-  header_lines += 1 if row[ISO_D] == ISO_D
-  lines += 1
+    description_only = [row[CA], row[NAICS], row[NCCI], row[SIC]].all?(&:nil?)
+    if description_only
+      # description ran long, append to prev row
+      description_types.each do |desc_type|
+        str = prev_row[desc_type]
+        str << "#{space(str)}#{row[desc_type]}" if row[desc_type]
+      end
+      # prev_row[GEN_D] << " #{gen_d}" if gen_d
+      # prev_row[ISO_D] << " #{iso_d}" if iso_d
+    end
 
-  # cases to account for
-  # 1. when a description contains a newline
-  #    a. it's either run long
-  #    b. or there are multiple descriptions mapping to different codes (should have matching newlines in codes)
-  # 2. when a description runs long and occupies a new row by itself
-  # 3. when an ISO Description maps to many other codes/descriptions and has white space on top and bottom
+    prev_row = row unless description_only
+    # cases to account for
+    # 1. when a description contains a newline
+    #    a. it's either run long
+    #    b. or there are multiple descriptions mapping to different codes (should have matching newlines in codes)
+    # 2. when a description runs long and occupies a new row by itself
+    # 3. when an ISO Description maps to many other codes/descriptions and has white space on top and bottom
+  end
 end
 
 if DEBUG_MODE
-  puts "number of lines read: #{lines}"
-  puts "number of header lines read: #{header_lines}"
+  puts "number of header lines read: #{header_lines} (of 83 expected)"
+  puts "number of regular lines read: #{non_header_lines} (of 2768 expected)"
+  puts "total lines read: #{lines} (of 2851 expected)"
   p systems
   p description_types
 end
