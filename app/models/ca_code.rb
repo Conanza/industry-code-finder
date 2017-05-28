0fea85a1e95b46350e27bab1966f4e76a6e64612
Conan Tzou <conan@asperasoft.com>
2017-05-27 20:34:57 -0700

    annotate models

--- app/models/ca_code.rb
+++ app/models/ca_code.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: ca_codes
+#
+#  id                 :integer          not null, primary key
+#  code_number        :string           not null
+#  iso_description_id :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+
 class CaCode < ApplicationRecord
 end
--- app/models/general_description.rb
+++ app/models/general_description.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: general_descriptions
+#
+#  id          :integer          not null, primary key
+#  description :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class GeneralDescription < ApplicationRecord
 end
