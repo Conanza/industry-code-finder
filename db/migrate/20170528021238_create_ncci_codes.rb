69441db684ce9b6fcaf389508f47625d09ed8e9e
Conan Tzou <conan@asperasoft.com>
2017-05-27 19:48:45 -0700

    add ncci_code migration

--- db/migrate/20170528021238_create_ncci_codes.rb
+++ db/migrate/20170528021238_create_ncci_codes.rb
@@ -1,8 +1,11 @@
 class CreateNcciCodes < ActiveRecord::Migration[5.0]
   def change
     create_table :ncci_codes do |t|
-
+      t.string :code_number, null: false
+      t.belongs_to :iso_description, index: true
       t.timestamps
     end
+
+    add_index :ncci_codes, :code_number, unique: true
   end
 end
