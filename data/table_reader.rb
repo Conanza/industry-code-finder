e30f137b7bea49d087cb408fc18df3a9127d1dc6
Conan Tzou <conan@asperasoft.com>
2017-05-28 17:08:25 -0700

    complete first go at algorithm

--- data/table_reader.rb
+++ data/table_reader.rb
@@ -7,13 +7,9 @@ NAICS = 'NAICS'.freeze
 NCCI  = 'NCCI'.freeze
 SIC   = 'SIC'.freeze
 
-systems = [CA, NAICS, NCCI, SIC]
-
 # Description Types
 ISO_D = 'ISO Description'.freeze
 GEN_D = 'General Description'.freeze
-
-description_types = [ISO_D, GEN_D]
 
 # Matchers
 NA_REGEX  = /N\/A/
@@ -28,9 +24,10 @@ header_lines = 0
 lines = 0
 non_header_lines = 0
 CSV.open('./writer_test.csv', 'wb') do |csv|
+  completed_rows = []
   deferred_rows = []
   prev_row = nil
-  centered_row = nil
+  row_with_iso = nil
   CSV.foreach('./fastcompclasscodecrossreferenceguide.csv',
               headers: true,
               col_sep: ',',
@@ -44,28 +41,46 @@ CSV.open('./writer_test.csv', 'wb') do |csv|
     end
     non_header_lines += 1
 
-    if row[ISO_D].nil?
-      puts "no #{ISO_D}"
-      deferred_rows.push(row)
-    else
-      puts "#{ISO_D} exists"
-      row[ISO_D].gsub!("\n", " ")
+    description_only_row = [row[CA], row[NAICS],
+                            row[NCCI], row[SIC]].all?(&:nil?)
+    iso_empty = row[ISO_D].nil?
 
-      deffered_rows.length
-    end
-
-    description_only = [row[CA], row[NAICS], row[NCCI], row[SIC]].all?(&:nil?)
-    if description_only
-      # description ran long, append to prev row
-      description_types.each do |desc_type|
+    # description ran long, append to prev row
+    if description_only_row
+      [ISO_D, GEN_D].each do |desc_type|
         str = prev_row[desc_type]
         str << "#{space(str)}#{row[desc_type]}" if row[desc_type]
       end
-      # prev_row[GEN_D] << " #{gen_d}" if gen_d
-      # prev_row[ISO_D] << " #{iso_d}" if iso_d
+
+      next
     end
 
-    prev_row = row unless description_only
+    if iso_empty
+      if row_with_iso.nil?
+        deferred_rows.push(row)
+      else
+        row[ISO_D] = row_with_iso[ISO_D]
+        completed_rows << row
+      end
+    else
+      row[ISO_D].gsub!("\n", " ")
+
+      if deferred_rows.empty?
+        row_with_iso = nil
+      else
+        row_with_iso = row
+        deferred_rows.each do |deferred_row|
+          deferred_row[ISO_D] = row_with_iso[ISO_D]
+
+          completed_rows << deferred_row
+        end
+        deferred_rows = []
+      end
+
+      completed_rows << row
+    end
+
+    prev_row = row
     # cases to account for
     # 1. when a description contains a newline
     #    a. it's either run long
@@ -73,14 +88,16 @@ CSV.open('./writer_test.csv', 'wb') do |csv|
     # 2. when a description runs long and occupies a new row by itself
     # 3. when an ISO Description maps to many other codes/descriptions and has white space on top and bottom
   end
+
+  completed_rows.each do |row|
+    csv << row
+  end
 end
 
 if DEBUG_MODE
   puts "number of header lines read: #{header_lines} (of 83 expected)"
   puts "number of regular lines read: #{non_header_lines} (of 2768 expected)"
   puts "total lines read: #{lines} (of 2851 expected)"
-  p systems
-  p description_types
 end
 
 # NOTE
