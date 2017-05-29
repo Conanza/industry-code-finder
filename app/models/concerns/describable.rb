120971d023cd99147f89571eaf8f4956f03be5c9
Conan Tzou <conan@asperasoft.com>
2017-05-28 23:21:09 -0700

    update associations in Describable

--- app/models/concerns/describable.rb
+++ app/models/concerns/describable.rb
@@ -5,5 +5,10 @@ module Describable
     has_many :mappings, dependent: :nullify
     has_many :general_descriptions, through: :mappings
     has_many :iso_descriptions, through: :mappings
+
+    [:ca_codes, :naics_codes, :ncci_codes, :sic_codes].each do |code|
+      next if code == name.underscore.pluralize.to_sym
+      has_many code, through: :mappings
+    end
   end
 end
